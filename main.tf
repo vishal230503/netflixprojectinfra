ace9e1220eec68aec3c7945a2e23949d475a8a16
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2024-10-23 16:19:12 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+count = 4
+ami = "ami-0ddc798b3f1a5117e"
+instance_type = "t2.medium"
+key_name = "rahull"
+vpc_security_groups_ids = ["sg-05f044979e305302e"]
+tags = {
+Name = var.instance_names[count.index]
+}
+}
+
+variable "instance_names" {
+default = ["jenkins", "nexus", "tomcat-1", "tomcat-2"]
+}
